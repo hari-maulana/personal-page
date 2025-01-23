d23815b32b68ed77a8a8e17cc2789414aa8168ba
hari-maulana <harimaulana.xyz@gmail.com>
2025-01-23 10:36:37 +0700

    style: style the navbar font and bg

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,32 +5,53 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-      <nav>
-        <a href="">
-          <Image src={assets.logo} className="w-28 cursor-pointer mr-14" />
+      <nav className="w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50">
+        <a href="#top">
+          <Image
+            alt="logo"
+            src={assets.logo}
+            className="w-28 cursor-pointer mr-14"
+          />
         </a>
 
-        <ul>
+        <ul
+          className="hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3
+        bg-white shadow-sm bg-opacity-50"
+        >
           <li>
-            <a href="#top">Home</a>
+            <a href="#top" className="font-Ovo">
+              Home
+            </a>
           </li>
           <li>
-            <a href="#about">About me</a>
+            <a href="#about" className="font-Ovo">
+              About me
+            </a>
           </li>
           <li>
-            <a href="#services">Services</a>
+            <a href="#services" className="font-Ovo">
+              Services
+            </a>
           </li>
           <li>
-            <a href="#work">My Work</a>
+            <a href="#work" className="font-Ovo">
+              My Work
+            </a>
           </li>
           <li>
-            <a href="#contact">Contact me</a>
+            <a href="#contact" className="font-Ovo">
+              Contact me
+            </a>
           </li>
         </ul>
 
         <div>
-          <a href="#contact">
-            Contact <Image src={assets.arrow_icon} className="w-3" />
+          <a
+            href="#contact"
+            className="hidden font-Ovo lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4"
+          >
+            Contact{" "}
+            <Image alt="arrow_icon" src={assets.arrow_icon} className="w-3" />
           </a>
         </div>
       </nav>
