03da0be36eb7662cb722d36608041389e8c13c62
hari-maulana <harimaulana.xyz@gmail.com>
2025-01-24 22:04:39 +0700

    feat: header section

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,21 @@
 import { assets } from "@/assets/assets";
 import Image from "next/image";
-import React from "react";
+import React, { useRef } from "react";
 
 const Navbar = () => {
+  const sideMenuRef = useRef(null);
+  const openMenu = () => {
+    if (sideMenuRef.current) {
+      sideMenuRef.current.style.transform = "translateX(-16rem)";
+    }
+  };
+
+  const closeMenu = () => {
+    if (sideMenuRef.current) {
+      sideMenuRef.current.style.transform = "translateX(16rem)";
+    }
+  };
+
   return (
     <>
       <div className="fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]">
@@ -65,7 +78,53 @@ const Navbar = () => {
             Contact{" "}
             <Image alt="arrow_icon" src={assets.arrow_icon} className="w-3" />
           </a>
+
+          <button className="block md:hidden ml-3" onClick={openMenu}>
+            <Image src={assets.menu_black} alt="menu button" className="w-6" />
+          </button>
         </div>
+
+        {/* mobile menu */}
+
+        <ul
+          ref={sideMenuRef}
+          className="flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64
+        z-50 h-screen bg-rose-50 transition duration-500"
+        >
+          <div className=" right-6 top-6 absolute" onClick={closeMenu}>
+            <Image
+              src={assets.close_black}
+              alt="close button"
+              className="w-5 cursor-pointer"
+            />
+          </div>
+
+          <li>
+            <a href="#top" className="font-Ovo" onClick={closeMenu}>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#about" className="font-Ovo" onClick={closeMenu}>
+              About me
+            </a>
+          </li>
+          <li>
+            <a href="#services" className="font-Ovo" onClick={closeMenu}>
+              Services
+            </a>
+          </li>
+          <li>
+            <a href="#work" className="font-Ovo" onClick={closeMenu}>
+              My Work
+            </a>
+          </li>
+          <li>
+            <a href="#contact" className="font-Ovo" onClick={closeMenu}>
+              Contact me
+            </a>
+          </li>
+        </ul>
       </nav>
     </>
   );
--- app/layout.js
+++ app/layout.js
@@ -19,7 +19,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${outfit.className} ${ovo.className} antialiased`}>
+      <body
+        className={`${outfit.className} ${ovo.className} antialiased leading-8 overflow-x-hidden`}
+        // leading increse line height
+      >
         {children}
       </body>
     </html>
